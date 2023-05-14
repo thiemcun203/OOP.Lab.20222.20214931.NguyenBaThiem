07ff1e94e565edcf38fff49b1d87906cb337378e
Nguyễn Bá Thiêm <104337976+thiemcun203@users.noreply.github.com>
2023-05-14 21:39:50 +0700

    delete

--- Lab01/firstdialog.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-public class firstdialog {
-    public static void main(String[] args){
-        JOptionPane.showMessageDialog(null,"hello world");
-        System.exit(0);
-    }
-}
