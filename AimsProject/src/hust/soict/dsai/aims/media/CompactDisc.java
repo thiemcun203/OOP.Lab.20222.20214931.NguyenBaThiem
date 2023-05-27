9673dad322f53092923e04f14988e8158da4d4c6
Nguyễn Bá Thiêm <104337976+thiemcun203@users.noreply.github.com>
2023-05-27 23:46:48 +0700

    new update for compact disc

--- AimsProject/src/hust/soict/dsai/aims/media/CompactDisc.java
+++ AimsProject/src/hust/soict/dsai/aims/media/CompactDisc.java
@@ -1,8 +1,40 @@
 package hust.soict.dsai.aims.media;
-
+import java.util.ArrayList;
 public class CompactDisc extends Disc{
+    private String artist;
+    private ArrayList<Track> authors = new ArrayList<Track>();
     public CompactDisc(int id,String title, String category, String director, int length, float cost) {
         super(id, title,category,cost,length, director);
     }
+    public String getArtist(){
+        return artist;
+    }
+    public void addTrack(Track newTrack){
+        if (authors.contains(newTrack)){
+            System.out.println("This track already has been in the list of tracks");
+        }
+        else{
+            authors.add(newTrack);
+        }
 
+    }
+    public void removeTrack(Track newTrack){
+        if (!authors.contains(newTrack)){
+            System.out.println("This track does not exist in the list of tracks");
+        }
+        else{
+            authors.remove(newTrack);
+        }
+    }
+    public int getLength(ArrayList<Track> cD) {
+        int totalLength = 0;
+    
+        for (Track track : cD) {
+            totalLength += track.getLength();
+        }
+    
+        return totalLength;
+    }
+    
 }
+
